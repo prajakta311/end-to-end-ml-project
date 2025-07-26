9c80ec170067ddb997d2307a0e0f0b9a6d3fd16a
PrajaktaN <prajaktanawale131@gmail.com>
2025-07-26 16:07:33 +0530

    added the logger file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages 
+
+setup(
+    name = "us_visa", 
+    version = "0.0.0", 
+    author = "Prajakta",
+    author_email = "prajaktanawale131@gmail.com",
+    packages = find_packages()
+)
